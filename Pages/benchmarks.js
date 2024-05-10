eb02b072e492ccda2a51841247659660241c552b
Tuna Okçu <tuna.okcu@ug.bilkent.edu.tr>
2024-05-10 08:06:52 +0300

    Chart

--- Pages/benchmarks.js
+++ Pages/benchmarks.js
@@ -3,7 +3,7 @@ import quickhull from "../Code/Algorithms/QuickHull.js";
 import graham from "../Code/Algorithms/Graham.js";
 import mergehull from "../Code/Algorithms/Mergehull.js";
 import { generatePointsGuassian, generatePointsUniform } from "../Code/Helper/Distributions.js";
-
+//import Chart from 'chart.js/auto';
 
 let algorithms = {
     jarvis: jarvis,
@@ -24,14 +24,145 @@ window.onload = () => {
         let end = parseInt(event.target.elements.endNumber.value);
         let delta = parseInt(event.target.elements.diff.value);
         
-        testAndLogResults(start, end, delta);
+        testAndChartResults(start, end, delta);
     });
 
 }
 
 function clearResults(){
-    document.getElementById("results").innerHTML = "";
+    document.getElementById("results").innerHTML = `<div style='width: 800px;'><canvas id='chart'></canvas></div>`;
 }
+
+function getChartData(start, end, delta){
+    let iterations = parseInt(Math.ceil((end-start) / parseFloat(delta)));
+    let pointsGaussian = generatePointsGuassian(end, 10);
+    //let pointsUniform = generatePointsUniform(end, 10);
+
+    let jarvisData = [];
+    let grahamData = [];
+    let quickhullData = [];
+
+    let curJarvis, curGraham, curQuickhull;
+
+    for (let i = 0; i < iterations; i++){
+        start += delta;
+
+        //Cap at end
+        start = start > end ? end : start;
+
+        [curJarvis, curGraham, curQuickhull] = testChart(start, pointsGaussian.slice(start));//, pointsUniform.slice(start));
+        jarvisData.push(curJarvis);
+        grahamData.push(curGraham);
+        quickhullData.push(curQuickhull);
+    }   
+
+    console.log(jarvisData);
+    console.log(grahamData);
+    console.log(quickhullData);
+
+
+
+    return [jarvisData, grahamData, quickhullData];
+}
+
+function testChart(numberOfPoints, points){
+    return [
+        {x: numberOfPoints, y: time("jarvis", points)},
+        {x: numberOfPoints, y: time("graham", points)},
+        {x: numberOfPoints, y: time("quickhull", points)}
+    ]
+}
+function testAndChartResults(start, end, delta){
+    
+    //let htmlElem = "<div style='width: 800px;''><canvas id='chart'></canvas></div>";
+    let htmlElem = document.getElementById("chart");
+
+    let [jarvisData, grahamData, quickhullData] = getChartData(start, end, delta);
+    
+    console.log(jarvisData, grahamData, quickhullData)
+
+
+
+
+
+
+  // <block:setup:1>
+  const DATA_COUNT = 7;
+  const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
+  
+  const data = {
+    datasets: [
+      {
+        label: 'Jarvis',
+        data: jarvisData,
+        yAxisID: 'y',
+      },
+      {
+        label: 'Graham',
+        data: grahamData,
+        yAxisID: 'y1',
+      },
+      {
+        label: 'Quickhull',
+        data: quickhullData,
+        yAxisID: 'y2',
+      }
+    ]
+  };
+  // </block:setup>
+  
+  // <block:config:0>
+  const config = {
+    type: 'line',
+    data: data,
+    options: {
+      responsive: true,
+      interaction: {
+        mode: 'index',
+        intersect: false,
+      },
+      stacked: false,
+      plugins: {
+        title: {
+          display: true,
+          text: 'Chart.js Line Chart - Multi Axis'
+        }
+      },
+      scales: {
+        y: {
+          type: 'linear',
+          display: true,
+          position: 'left',
+        },
+        y1: {
+          type: 'linear',
+          display: true,
+          position: 'right',
+  
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+        y2: {
+            type: 'linear',
+            display: true,
+            position: 'right',
+    
+            // grid line settings
+            grid: {
+              drawOnChartArea: false, // only want the grid lines for one axis to show up
+            },
+          }
+      }
+    },
+  };
+
+  new Chart(htmlElem, config);
+
+
+}
+
 
 function testAndLogResults(start, end, delta){
     let iterations = parseInt(Math.ceil((end-start) / parseFloat(delta)));
@@ -81,5 +212,5 @@ function time(algoName, points){
 
        let dur = (end - start);
 
-       return parseInt(dur)+ " ms";
+       return parseInt(dur);
 }
